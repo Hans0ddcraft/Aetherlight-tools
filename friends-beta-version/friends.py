c498e90f61a10a4f744164c48081db2451be6e9a
Hans0ddcraft <43818889+Hans0ddcraft@users.noreply.github.com>
2019-01-15 11:02:54 -0600

    Added file creation in same directory

--- friends-beta-version/friends.py
+++ friends-beta-version/friends.py
@@ -1,8 +1,13 @@
 #Import os module used to find the AppData directory
 import os
+import sys
 
 #Open the text file. This block creates the file if I didn't previously exist or runs it if the Aetherlight session is over
-friends = open(f"{os.getcwd()}/friends.txt","a+")
+
+file = sys.argv[0]
+pathname = os.path.dirname(file)
+
+friends = open(f"{os.path.abspath(pathname)}friends.txt","a+")
 friends.read()
 friends.close()
 
@@ -17,12 +22,12 @@ while True:
     #Split the string into a list where the name occures 
     data = data.split('Name recorded as')
     #Open the friends file
-    friends = open(f"{os.getcwd()}/friends.txt","a+")
+    friends = open(f"{os.path.abspath(pathname)}/friends.txt","a+")
     #Iterate through the output file
     for i in data:
         #Identify the names
         if '||' in i:
-            if i[i.index('|')+5:i.index('\n')] not in open(f"{os.getcwd()}/friends.txt","r").read().split('\n'):
+            if i[i.index('|')+5:i.index('\n')] not in open(f"{os.path.abspath(pathname)}/friends.txt","r").read().split('\n'):
                 #Append the names to the text file
                 friends.write(i[i.index('|')+5:i.index('\n')]+'\n')
                 people.append(i[i.index('|')+5:i.index('\n')])
